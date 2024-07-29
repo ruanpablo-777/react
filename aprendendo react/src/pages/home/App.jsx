ef708bfd5a79b67d62a45d46a499fa808d280564
RuanPablo-7 <ToyaMON777@outlook.com>
2024-07-29 17:15:01 -0300

    update

--- aprendendo react/src/pages/home/App.jsx
+++ aprendendo react/src/pages/home/App.jsx
@@ -6,20 +6,44 @@ import api from "../../services/api"
 function Home() {
   const [users, setUsers] = useState([])
 
-  const imputName = useRef()
-  const imputage = useRef()
-  const imputemail = useRef()
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
 
   async function getUsers() {
     const usersFromApi = await api.get("/usuarios")
 
     setUsers(usersFromApi.data)
-    console.log(users)
+    // console.log(users)
   }
   async function createUsers() {
-    const usersFromApi = await api.get("/usuarios")
-    console.log(inputname)
+    console.log(inputAge)
+    const name =  inputName.current.value
+    const Age = inputAge.current.value
+    const email = inputEmail.current.value
+    if(!name || !Age || !email){
+      alert("Por favor, preencha todos os campos")
+      return
+    }
+    await api.post("/usuarios", {
+      name: name,
+      age: Age,
+      email: email
+      
+    })
+    
+    
 
+    getUsers()
+    inputName.current.value = ""
+    inputAge.current.value = ""
+    inputEmail.current.value = ""
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`)
+
+    getUsers()
   }
 
   useEffect(() => {
@@ -29,25 +53,28 @@ function Home() {
 
   return (
 
-   
+
 
     <div className="container">
       <form action="">
-        <h1>Cadastro de Usuarios</h1>
-        <input name="nome" type="text" placeholder="Nome" ref={inputName} />
-        <input name="idade" type="number" placeholder="Idade" ref={inputage} />
-        <input name="email" type="email" placeholder="Email" ref={inputemail} />
-        <button onClick={createUsers}>Adicionar</button>
+        <div>
+          <h1>Cadastro de Usuarios</h1>
+          <div id="loading"></div>
+        </div>
+        <input id="name" name="nome" type="text" placeholder="Nome" ref={inputName} />
+        <input name="idade" type="number" placeholder="Idade" ref={inputAge} />
+        <input name="email" type="email" placeholder="Email" ref={inputEmail} />
+        <button type="button" id="cadastrar" onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map((user) => (
         <div className="users" key={user.id}>
-          <div>
+          <div>{console.log(user)}
             <p>Nome: {user.name}</p>
-            <p>Idade: {user.idade}</p>
+            <p>Idade: {user.age}</p>
             <p>Email: {user.email}</p>
           </div>
-          <button>
+          <button onClick={() => deleteUsers(user.id)}>
             <img src={Trash} alt="" />
           </button>
         </div>
