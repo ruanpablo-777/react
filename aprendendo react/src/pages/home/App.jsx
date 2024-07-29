0b2e0b9d6cd82e60d4e3c46bfc16817e05415562
RuanPablo-7 <ToyaMON777@outlook.com>
2024-07-29 13:33:34 -0300

    update

--- aprendendo react/src/pages/home/App.jsx
+++ aprendendo react/src/pages/home/App.jsx
@@ -1,35 +1,47 @@
+import { useEffect, useState, useRef } from "react"
 import "./app.css"
 import Trash from "../../assets/Trash.svg"
+import api from "../../services/api"
 
 function Home() {
+  const [users, setUsers] = useState([])
 
-  const users = [
-    {
-      id: 1,
-      name: "Ruan",
-      idade: 18,
-      email: "ruan@gmail.com"
-    }, {
-      id: 2,
-      name: "Carla",
-      idade: 29,
-      email: "carla@gmail.com"
+  const imputName = useRef()
+  const imputage = useRef()
+  const imputemail = useRef()
 
-    }]
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios")
+
+    setUsers(usersFromApi.data)
+    console.log(users)
+  }
+  async function createUsers() {
+    const usersFromApi = await api.get("/usuarios")
+    console.log(inputname)
+
+  }
+
+  useEffect(() => {
+    getUsers()
+
+  }, [])
 
   return (
+
+   
 
     <div className="container">
       <form action="">
         <h1>Cadastro de Usuarios</h1>
-        <input name="nome" type="text" placeholder="Nome"/>
-        <input name="idade" type="number" placeholder="Idade"/>
-        <input name="email" type="email" placeholder="Email"/>
-        <button>Adicionar</button>
+        <input name="nome" type="text" placeholder="Nome" ref={inputName} />
+        <input name="idade" type="number" placeholder="Idade" ref={inputage} />
+        <input name="email" type="email" placeholder="Email" ref={inputemail} />
+        <button onClick={createUsers}>Adicionar</button>
       </form>
 
-      {users.map((user) => (       
-        <div className="users"  key={user.id}>
+      {users.map((user) => (
+        <div className="users" key={user.id}>
           <div>
             <p>Nome: {user.name}</p>
             <p>Idade: {user.idade}</p>
